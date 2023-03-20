bf7c980b91ecd393e2b45ae2c4cd729b7394de49
Yggdrasill <yggdrasill@shitpost.is>
2023-03-20 07:29:56 +0000

    Add code to ask the device about feature IDs
            To ask a device about a particular feature ID and its index, you
            have to query it with the IRoot feature. This feature is
            guaranteed to be at index 0. It takes the feature ID in
            big-endian byte order, and returns its feature index and type.

--- hidpp.c
+++ hidpp.c
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define TEST 1
+
 #define VENDOR_ID   0x046D
 #define PRODUCT_ID  0xC08F
 
@@ -28,6 +30,7 @@ int main(void)
   int fd;
   int bytes;
 
+#if TEST
   msg.hidpp.type = 0x11;    // long message
   msg.hidpp.index = 0xFF;   // wired mouse or transceiver, otherwise 1-6
   msg.hidpp.cmd = 0x0E;     // LED control
@@ -36,15 +39,26 @@ int main(void)
   msg.hidpp.args[1] = 0x01; // mode, solid color
   /* next three lines are colours */
   msg.hidpp.args[2] = 0xFF;
-  msg.hidpp.args[3] = 0xFF;
-  msg.hidpp.args[4] = 0xFF;
+  msg.hidpp.args[3] = 0x00;
+  msg.hidpp.args[4] = 0x00;
   msg.hidpp.args[5] = 0x02; // G Hub sends this, for what reason I do not know.
+#else
+  msg.hidpp.type = 0x10;
+  msg.hidpp.index = 0xFF;
+  msg.hidpp.cmd = 0x00;
+  msg.hidpp.func = 0x0E;
+  msg.hidpp.args[0] = 0x80;
+  msg.hidpp.args[1] = 0x60;
+#endif
 
   fd = open("/dev/hidraw1", O_RDWR);
 
   write(fd, &msg, sizeof(msg) );
+
+#if TEST
   msg.data[4] = 0x01;       // LED 1
   write(fd, &msg, sizeof(msg) );
+#endif
 
   bytes = read(fd, &response, sizeof(response) );
 
