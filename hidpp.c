504e4227c36e795630fbc4e40a30f7a759c3941e
Yggdrasill <yggdrasill@shitpost.is>
2023-03-20 06:32:36 +0000

    Write basic program to set LED color (solid)
            This commit adds a program to write LED colours to the device.
            The path to the hidraw file is currently a string literal, and
            will be replaced with libudev in the future.

---
+++ hidpp.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#define VENDOR_ID   0x046D
+#define PRODUCT_ID  0xC08F
+
+struct _hidpp_msg {
+  uint8_t type;
+  uint8_t index;
+  uint8_t cmd;
+  uint8_t func;
+  uint8_t args[16];
+};
+
+union hidpp_msg {
+  struct _hidpp_msg hidpp;
+  uint8_t data[20];
+};
+
+int main(void)
+{
+  union hidpp_msg msg = { 0 };
+  int fd;
+
+  msg.hidpp.type = 0x11;    // long message
+  msg.hidpp.index = 0xFF;   // wired mouse or transceiver, otherwise 1-6
+  msg.hidpp.cmd = 0x0E;     // LED control
+  msg.hidpp.func = 0x3E;    // Byte sent by G Hub for solid LED colours
+  msg.hidpp.args[0] = 0x00; // LED 0
+  msg.hidpp.args[1] = 0x01; // mode, solid color
+  /* next three lines are colours */
+  msg.hidpp.args[2] = 0xFF;
+  msg.hidpp.args[3] = 0xFF;
+  msg.hidpp.args[4] = 0xFF;
+  msg.hidpp.args[5] = 0x02; // G Hub sends this, for what reason I do not know.
+
+  fd = open("/dev/hidraw1", O_RDWR);
+
+  write(fd, &msg, sizeof(msg) );
+  msg.data[4] = 0x01;       // LED 1
+  write(fd, &msg, sizeof(msg) );
+
+  return 0;
+}
