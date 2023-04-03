7fc965f6b1f29e733add17c92901957032ea5395
Yggdrasill <yggdrasill@shitpost.is>
2023-04-04 00:55:48 +0100

    Add new defined constants and rename struct vars
            This commit adds some new defined constants for the message
            type, which is the first byte of a HID++ message. It also
            defines some constants for the length of these message types,
            which is 7 and 20 bytes respectively. Finally, it adds a SWID
            constant, which is the software ID of our program attached to
            any function calls.
    
            It also renames _hidpp_msg's index to devidx and func to fcnt.

--- hidpp.c
+++ hidpp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <fcntl.h>
@@ -7,14 +8,22 @@
 
 #define TEST 0
 
-#define VENDOR_ID   0x046D
-#define PRODUCT_ID  0xC08F
+#define VENDOR_ID       0x046D
+#define PRODUCT_ID      0xC08F
+
+#define MSG_SHORT       0x10
+#define MSG_LONG        0x11
+
+#define MSG_SHORT_LEN   7
+#define MSG_LONG_LEN    20
+
+#define MSG_SWID        0x0A
 
 struct _hidpp_msg {
   uint8_t type;
-  uint8_t index;
+  uint8_t devidx;
   uint8_t cmd;
-  uint8_t func;
+  uint8_t fcnt;
   uint8_t args[16];
 };
 
@@ -33,9 +42,9 @@ int main(void)
 
 #if TEST
   msg.hidpp.type = 0x11;    // long message
-  msg.hidpp.index = 0xFF;   // wired mouse or transceiver, otherwise 1-6
+  msg.hidpp.devidx = 0xFF;   // wired mouse or transceiver, otherwise 1-6
   msg.hidpp.cmd = 0x0E;     // LED control
-  msg.hidpp.func = 0x3E;    // Byte sent by G Hub for solid LED colours
+  msg.hidpp.fcnt = 0x3E;    // Byte sent by G Hub for solid LED colours
   msg.hidpp.args[0] = 0x00; // LED 0
   msg.hidpp.args[1] = 0x01; // mode, solid color
   /* next three lines are colours */
@@ -45,9 +54,9 @@ int main(void)
   msg.hidpp.args[5] = 0x02; // G Hub sends this, for what reason I do not know.
 #else
   msg.hidpp.type = 0x10;
-  msg.hidpp.index = 0xFF;
+  msg.hidpp.devidx = 0xFF;
   msg.hidpp.cmd = 0x00;
-  msg.hidpp.func = 0x0E;
+  msg.hidpp.fcnt = 0x0E;
   msg.hidpp.args[0] = 0x00;
   msg.hidpp.args[1] = 0x01;
 #endif
@@ -78,7 +87,7 @@ int main(void)
   }
   printf("\n-----------------------------------------------------------\n");
 
-  msg.hidpp.func |= (1 << 4);
+  msg.hidpp.fcnt |= (1 << 4);
   for(int i = 0; i < features; i++) {
     msg.hidpp.args[0] = i;
     write(fd, &msg, sizeof(msg) );
