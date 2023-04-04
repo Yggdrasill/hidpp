afdd46462d327ae99e46d17e6c50ed6cd98467d9
Yggdrasill <yggdrasill@shitpost.is>
2023-04-04 03:20:34 +0100

    Modify main() to make use of new functions
            For now, main() just calls the cmds_print() and exits.

--- hidpp.c
+++ hidpp.c
@@ -186,67 +186,17 @@ int main(void)
 {
   union hidpp_msg msg = { 0 };
   union hidpp_msg response = { 0 };
+  char *name;
   int fd;
   int bytes;
   int features;
-
-#if TEST
-  msg.hidpp.type = 0x11;    // long message
-  msg.hidpp.devidx = 0xFF;   // wired mouse or transceiver, otherwise 1-6
-  msg.hidpp.cmd = 0x0E;     // LED control
-  msg.hidpp.fcnt = 0x3E;    // Byte sent by G Hub for solid LED colours
-  msg.hidpp.args[0] = 0x00; // LED 0
-  msg.hidpp.args[1] = 0x01; // mode, solid color
-  /* next three lines are colours */
-  msg.hidpp.args[2] = 0xFF;
-  msg.hidpp.args[3] = 0x00;
-  msg.hidpp.args[4] = 0x00;
-  msg.hidpp.args[5] = 0x02; // G Hub sends this, for what reason I do not know.
-#else
-  msg.hidpp.type = 0x10;
-  msg.hidpp.devidx = 0xFF;
-  msg.hidpp.cmd = 0x00;
-  msg.hidpp.fcnt = 0x0E;
-  msg.hidpp.args[0] = 0x00;
-  msg.hidpp.args[1] = 0x01;
-#endif
+  uint16_t id;
+  uint8_t index;
+  uint8_t type;
 
   fd = open("/dev/hidraw1", O_RDWR);
 
-  write(fd, &msg, sizeof(msg) );
-
-#if TEST
-  msg.data[4] = 0x01;       // LED 1
-  write(fd, &msg, sizeof(msg) );
-#endif
-
-  bytes = read(fd, &response, sizeof(response) );
-
-  for(int i = 0; i < bytes; i++) {
-    printf("%02x ", response.data[i]);
-  }
-  puts("");
-
-  msg.hidpp.cmd = response.hidpp.args[0];
-  write(fd, &msg, sizeof(msg) );
-  bytes = read(fd, &response, sizeof(response) );
-  features = response.hidpp.args[0];
-
-  for(int i = 0; i < bytes; i++) {
-    printf("%02x ", response.data[i]);
-  }
-  printf("\n-----------------------------------------------------------\n");
-
-  msg.hidpp.fcnt |= (1 << 4);
-  for(int i = 0; i < features; i++) {
-    msg.hidpp.args[0] = i;
-    write(fd, &msg, sizeof(msg) );
-    bytes = read(fd, &response, sizeof(response) );
-    for(int j = 0; j < bytes; j++) {
-      printf("%02x ", response.data[j]);
-    }
-    puts("");
-  }
+  cmds_print(fd);
 
   return 0;
 }
